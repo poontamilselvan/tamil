88aa078748c2395830a62cc207a096f56c9d75b4
poontamilselvan <31534970+poontamilselvan@users.noreply.github.com>
2018-01-21 13:27:15 +0530

    Create pali.c

---
+++ pali.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  int n = 12321;
+    isPal(n)? printf("Yesn"): printf("Non");
+  n = 12;
+    isPal(n)? printf("Yesn"): printf("Non");
+  n = 88;
+    isPal(n)? printf("Yesn"): printf("Non");
+  n = 8999;
+    isPal(n)? printf("Yesn"): printf("Non");
+      return 0;}
