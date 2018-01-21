0e4f83343968ff489985cb0f9ae40ae953de2061
poontamilselvan <31534970+poontamilselvan@users.noreply.github.com>
2018-01-21 13:19:09 +0530

    Update odd.c

--- odd.c
+++ odd.c
@@ -2,10 +2,8 @@
 int main()
 { int number;
  printf("Enter an integer: ");
-    scanf("%d", &number);
-    if(number % 2 == 0)
+    scanf("%d", &number); if(number % 2 == 0)
         printf("%d is even.", number);
     else
         printf("%d is odd.", number);
-  return 0;
-}
+  return 0;}
