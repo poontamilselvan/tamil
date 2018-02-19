ce61731b96ea964e612bd2cdd4649aa3f6e26e1d
poontamilselvan <31534970+poontamilselvan@users.noreply.github.com>
2018-02-19 16:52:00 +0530

    Update fiboo.c

--- fiboo.c
+++ fiboo.c
@@ -6,7 +6,7 @@ void main()
      int c,i,n;
      printf ("enter the limit");
      scanf("%d",&n);
-     for(i=0;i<=n;i++)
+     for(i=0;i<=n;i++)xfgdhhjjdgjdghdghghghghj
      {
      c=a+b;
      printf("%d",c);
