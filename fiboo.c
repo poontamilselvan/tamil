fbb502ce420629e4d25231d88f62bd36362bdaa1
poontamilselvan <31534970+poontamilselvan@users.noreply.github.com>
2018-02-19 16:49:48 +0530

    Create fiboo.c

---
+++ fiboo.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+     int a=0;
+     int b=1;
+     int c,i,n;
+     printf ("enter the limit");
+     scanf("%d",&n);
+     for(i=0;i<=n;i++)
+     {
+     c=a+b;
+     printf("%d",c);
+     a=b;
+     b=c;
+     }
+     
+} 
