2306ef0bf426ec9060f001cc2ba0b6817666a51d
poontamilselvan <31534970+poontamilselvan@users.noreply.github.com>
2018-02-19 16:19:57 +0530

    Create graetwest.c

---
+++ graetwest.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+     int a[10];
+     int i;
+     int l;
+     printf("enter the number");
+     for(i=0;i<=10;i++)
+     scanf("%d\n",&a[i]);
+     l=a[0];
+     for(i=0;i<=10;i++)
+     {
+         if(a[i]>l)
+         {
+         l=a[i];
+          printf("the greatest is%d",l);
+         }
+        
+     }
+     
+    
+}
