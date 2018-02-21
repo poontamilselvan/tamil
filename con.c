72fe4d6cae67314ac04bd4ab84977c79b75d0681
poontamilselvan <31534970+poontamilselvan@users.noreply.github.com>
2018-02-21 16:39:36 +0530

    Create con.c

---
+++ con.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char a[10],b[10];
+    printf("enter the 1st word");
+    scanf("%s",&a);
+    
+    printf("enter the 2nd word");
+    scanf("%s",&b);
+    printf("%s%s",a,b);
+    
+}
