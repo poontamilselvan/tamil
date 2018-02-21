60da17103b98ded99c3895ebfc0fb5e65336fd7b
poontamilselvan <31534970+poontamilselvan@users.noreply.github.com>
2018-02-21 16:13:57 +0530

    Create ntime.c

---
+++ ntime.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char a[190];
+    int i;
+    printf("enter the name");
+    scanf("%s",&a);
+    for(i=0;i<=100;i++)
+    printf("%s\t",a);
+    
+}
