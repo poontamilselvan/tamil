e020f5dc2a0a0d80149e31edf7187e2b9fd87ced
poontamilselvan <31534970+poontamilselvan@users.noreply.github.com>
2018-03-07 16:17:44 +0530

    Create 53.c

---
+++ 53.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+  int a[100],i,d=0,n=5;
+  for(i=0;i<=n;i++)
+  {
+  scanf("%d",&a[i]);
+  }
+   for(i=0;i<=n;i++)
+   { 
+       d=d+a[i];    
+  
+ }
+ printf("%d",d);
+}
