8a6c409fc934bfbaf9881cac3b6a801948fff210
poontamilselvan <31534970+poontamilselvan@users.noreply.github.com>
2018-03-07 16:37:19 +0530

    Create 55.c

---
+++ 55.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+  int a,i,d=0;int n;
+  scanf("%d %d",&a,&i);
+  d=a*i;
+       if(n%2==0)
+         printf("even");
+       else
+       {
+               printf("odd");
+               
+       }
+    
+ }
+ 
