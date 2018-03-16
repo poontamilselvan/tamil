4488d10b1e8c1e1864f6fd280400ef94598d7f0d
poontamilselvan <31534970+poontamilselvan@users.noreply.github.com>
2018-03-16 17:00:33 +0530

    Create 66.c

---
+++ 66.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include<string.h>
+
+int main()
+{
+   int a[10],b
+   printf("enter the num");
+   scanf("%d",&a[i]);
+   b=a[i];
+   for(i=2;i<=b;i++)
+   {
+           if(b%i==0)
+           {
+           flag=0;
+           }
+   }
+   if(flag==0)
+   {
+           printf("not prime");
+   }
+   else
+   {
+           printf("prime");
+   }
+   return 0;
+}
