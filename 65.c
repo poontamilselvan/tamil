cb6f24794d15f9ac24968fe1116d6be196fb46f6
poontamilselvan <31534970+poontamilselvan@users.noreply.github.com>
2018-03-16 16:44:44 +0530

    Create 65.c

---
+++ 65.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<string.h>
+
+int main()
+{
+   char a[10];int i,m=0;
+   printf(" enter the value\n");
+   gets(a);
+   if(m==0)
+   {
+           printf("%s",a);
+   }
+   return 0;
+}
