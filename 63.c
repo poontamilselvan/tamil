2e5000e3ea00c2570654a2e0f46a513853add190
poontamilselvan <31534970+poontamilselvan@users.noreply.github.com>
2018-03-16 16:27:47 +0530

    Create 63.c

---
+++ 63.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<string.h>
+
+int main()
+{
+   char a[20];int i,c=1;
+   printf("enter the name\n");
+   scanf("%s",&a[i]);
+   for(i=0;i<=20;i++)
+   {
+           if(isspace (a[i]))
+           c++;
+   }
+   printf("%d",c);
+    return 0;
+}
