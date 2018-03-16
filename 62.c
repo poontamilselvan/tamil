e20c59c8ce6a471e1b7eb3a6395b17ef20be736b
poontamilselvan <31534970+poontamilselvan@users.noreply.github.com>
2018-03-16 15:56:58 +0530

    Rename 61.c to 62.c

---
+++ 62.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    char a[20];
+    int n,i;
+    printf("enter the name\n");
+    scanf("%s",&a[i]);
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+            printf("%c",a[i]);
+    }
+    return 0;
+}
