22b89dd5781b752cad30890f764c0f1f83f0ce33
poontamilselvan <31534970+poontamilselvan@users.noreply.github.com>
2018-03-17 12:13:21 +0530

    Create 67.c

---
+++ 67.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+        int a,n,c;
+        printf("enter num");
+        scanf("%d",&a);
+        n=a/10;
+        c=((n+1)*10);
+        printf("%d",c);
+        return 0;
+        
+        
+}
